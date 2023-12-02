52b8a079ff2c2d85d5e705cfa566ee04a82a4884
Oliver Gatland <gatlandollie@gmail.com>
2023-12-02 16:40:58 +0100

    Changing weather function into a class

--- Libraries/fplibrary/feature_engineering/feature_engineering_functions.py
+++ Libraries/fplibrary/feature_engineering/feature_engineering_functions.py
@@ -141,94 +141,100 @@ class Encoding:
 # pip install openmeteo-requests
 # pip install requests-cache retry-requests numpy pandas
 
-def obtain_weather(data: pd.DataFrame, start_date: str='2022-11-29', end_date: str='2023-11-28'):
-    """
-    Obtain weather data from OpenMeteo for a given date range and integrate it into a DataFrame.
 
-    Parameters:
-    - data: pd.DataFrame, a DataFrame containing latitude and longitude information.
-    - start_date: str, the start date for weather data (default: '2022-11-29').
-    - end_date: str, the end date for weather data (default: '2023-11-28').
+class WeatherFeatures:
+    
+    def __init__(self, data):
+        self.data = data
+    
+    def obtain_weather(self, start_date: str='2022-11-29', end_date: str='2023-11-28'):
+        """
+        Obtain weather data from OpenMeteo for a given date range and integrate it into a DataFrame.
 
-    Returns:
-    - pd.DataFrame, an updated dataset with integrated weather information, including max and min temperatures, average temperature, max precipitation, total precipitation, rainy days, and max wind speed.
-    """
+        Parameters:
+        - data: pd.DataFrame, a DataFrame containing latitude and longitude information.
+        - start_date: str, the start date for weather data (default: '2022-11-29').
+        - end_date: str, the end date for weather data (default: '2023-11-28').
 
-    data['lat']=round(data['latitude'],0)
-    data['lon']=round(data['longitude'],0)
-    coord_list = data[['lat','lon']].drop_duplicates().values.tolist()
+        Returns:
+        - pd.DataFrame, an updated dataset with integrated weather information, including max and min temperatures, average temperature, max precipitation, total precipitation, rainy days, and max wind speed.
+        """
 
-    # Setup the Open-Meteo API client with cache and retry on error
-    cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
-    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-    openmeteo = openmeteo_requests.Client(session = retry_session)
+        self.data['lat']=round(self.data['latitude'],0)
+        self.data['lon']=round(self.data['longitude'],0)
+        coord_list = self.data[['lat','lon']].drop_duplicates().values.tolist()
 
-    # Make sure all required weather variables are listed here
-    # The order of variables in hourly or daily is important to assign them correctly below
-    url = "https://archive-api.open-meteo.com/v1/archive"
+        # Setup the Open-Meteo API client with cache and retry on error
+        cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+        openmeteo = openmeteo_requests.Client(session = retry_session)
 
-    data['max_temp'] = 0
-    data['min_temp'] = 0
-    data['avg_temp'] = 0
-    data['max_precip'] = 0
-    data['total_precip'] = 0
-    data['rainy_days'] = 0
-    data['max_wind'] = 0
+        # Make sure all required weather variables are listed here
+        # The order of variables in hourly or daily is important to assign them correctly below
+        url = "https://archive-api.open-meteo.com/v1/archive"
 
-    # Create a loop to look for every coordination data
-    for i in range(len(coord_list)):
-        params = {
-            "latitude": coord_list[i][0],
-            "longitude": coord_list[i][1],
-            "start_date": start_date,
-            "end_date": end_date,
-            "daily": "temperature_2m_min,temperature_2m_max,precipitation_sum,wind_speed_10m_max"
-        }
-        responses = openmeteo.weather_api(url, params=params)
+        self.data['max_temp'] = 0
+        self.data['min_temp'] = 0
+        self.data['avg_temp'] = 0
+        self.data['max_precip'] = 0
+        self.data['total_precip'] = 0
+        self.data['rainy_days'] = 0
+        self.data['max_wind'] = 0
 
-        # Process first location. Add a for-loop for multiple locations or weather models
-        response = responses[0]
+        # Create a loop to look for every coordination data
+        for i in range(len(coord_list)):
+            params = {
+                "latitude": coord_list[i][0],
+                "longitude": coord_list[i][1],
+                "start_date": start_date,
+                "end_date": end_date,
+                "daily": "temperature_2m_min,temperature_2m_max,precipitation_sum,wind_speed_10m_max"
+            }
+            responses = openmeteo.weather_api(url, params=params)
 
-        # Process hourly data. The order of variables needs to be the same as requested.
-        daily = response.Daily()
-        temperature_2m_min = daily.Variables(0).ValuesAsNumpy()
-        temperature_2m_max = daily.Variables(1).ValuesAsNumpy()
-        precipitation_sum = daily.Variables(2).ValuesAsNumpy()
-        wind_speed_10m_max = daily.Variables(3).ValuesAsNumpy()
+            # Process first location. Add a for-loop for multiple locations or weather models
+            response = responses[0]
 
-        daily_data = {"date": pd.date_range(
-            start = pd.to_datetime(daily.Time(), unit = "s"),
-            end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
-            freq = pd.Timedelta(seconds = daily.Interval()),
-            inclusive = "left"
-        )}
-        daily_data["min_temp"] = temperature_2m_min
-        daily_data["max_temp"] = temperature_2m_max
-        daily_data["temp"] = (daily_data["min_temp"]+daily_data["max_temp"])*0.5
-        daily_data["precipitation"] = precipitation_sum
-        daily_data["max_wind_speed"] = wind_speed_10m_max
+            # Process hourly data. The order of variables needs to be the same as requested.
+            daily = response.Daily()
+            temperature_2m_min = daily.Variables(0).ValuesAsNumpy()
+            temperature_2m_max = daily.Variables(1).ValuesAsNumpy()
+            precipitation_sum = daily.Variables(2).ValuesAsNumpy()
+            wind_speed_10m_max = daily.Variables(3).ValuesAsNumpy()
 
-        daily_dataframe = pd.DataFrame(data = daily_data)
-        max_temp = daily_dataframe['max_temp'].max()
-        min_temp = daily_dataframe['min_temp'].min()
-        avg_temp = round(daily_dataframe['temp'].mean(),2)
-        max_precip = round(daily_dataframe['precipitation'].max(),2)
-        total_precip = round(daily_dataframe['precipitation'].sum(),2)
-        rainy_days = daily_dataframe['precipitation'].apply(lambda x: 1 if x > 0 else 0).sum()
-        max_wind = daily_dataframe['max_wind_speed'].max()
+            daily_data = {"date": pd.date_range(
+                start = pd.to_datetime(daily.Time(), unit = "s"),
+                end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
+                freq = pd.Timedelta(seconds = daily.Interval()),
+                inclusive = "left"
+            )}
+            daily_data["min_temp"] = temperature_2m_min
+            daily_data["max_temp"] = temperature_2m_max
+            daily_data["temp"] = (daily_data["min_temp"]+daily_data["max_temp"])*0.5
+            daily_data["precipitation"] = precipitation_sum
+            daily_data["max_wind_speed"] = wind_speed_10m_max
 
-        # Update the original DataFrame with calculated weather statistics for each location
-        data['max_temp'] = np.where((data['lat']==coord_list[i][0]) & (data['lon']==coord_list[i][1]), max_temp, data['max_temp'])
-        data['min_temp'] = np.where((data['lat']==coord_list[i][0]) & (data['lon']==coord_list[i][1]), min_temp, data['min_temp'])
-        data['avg_temp'] = np.where((data['lat']==coord_list[i][0]) & (data['lon']==coord_list[i][1]), avg_temp, data['avg_temp'])
-        data['max_precip'] = np.where((data['lat']==coord_list[i][0]) & (data['lon']==coord_list[i][1]), max_precip, data['max_precip'])
-        data['total_precip'] = np.where((data['lat']==coord_list[i][0]) & (data['lon']==coord_list[i][1]), total_precip, data['total_precip'])
-        data['rainy_days'] = np.where((data['lat']==coord_list[i][0]) & (data['lon']==coord_list[i][1]), rainy_days, data['rainy_days'])
-        data['max_wind'] = np.where((data['lat']==coord_list[i][0]) & (data['lon']==coord_list[i][1]), max_wind, data['max_wind'])
+            daily_dataframe = pd.DataFrame(data = daily_data)
+            max_temp = daily_dataframe['max_temp'].max()
+            min_temp = daily_dataframe['min_temp'].min()
+            avg_temp = round(daily_dataframe['temp'].mean(),2)
+            max_precip = round(daily_dataframe['precipitation'].max(),2)
+            total_precip = round(daily_dataframe['precipitation'].sum(),2)
+            rainy_days = daily_dataframe['precipitation'].apply(lambda x: 1 if x > 0 else 0).sum()
+            max_wind = daily_dataframe['max_wind_speed'].max()
 
-    data.drop(columns=['lat','lon'], inplace=True)
+            # Update the original DataFrame with calculated weather statistics for each location
+            self.data['max_temp'] = np.where((self.data['lat']==coord_list[i][0]) & (self.data['lon']==coord_list[i][1]), max_temp, self.data['max_temp'])
+            self.data['min_temp'] = np.where((self.data['lat']==coord_list[i][0]) & (self.data['lon']==coord_list[i][1]), min_temp, self.data['min_temp'])
+            self.data['avg_temp'] = np.where((self.data['lat']==coord_list[i][0]) & (self.data['lon']==coord_list[i][1]), avg_temp, self.data['avg_temp'])
+            self.data['max_precip'] = np.where((self.data['lat']==coord_list[i][0]) & (self.data['lon']==coord_list[i][1]), max_precip, self.data['max_precip'])
+            self.data['total_precip'] = np.where((self.data['lat']==coord_list[i][0]) & (self.data['lon']==coord_list[i][1]), total_precip, self.data['total_precip'])
+            self.data['rainy_days'] = np.where((self.data['lat']==coord_list[i][0]) & (self.data['lon']==coord_list[i][1]), rainy_days, self.data['rainy_days'])
+            self.data['max_wind'] = np.where((self.data['lat']==coord_list[i][0]) & (self.data['lon']==coord_list[i][1]), max_wind, self.data['max_wind'])
 
-    return data
+        self.data.drop(columns=['lat','lon'], inplace=True)
+
+        return self.data
 
 
 
