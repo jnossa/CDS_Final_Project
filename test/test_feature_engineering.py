848a11cb7e5fa766975e5d9977bb994795810bf8
viktoriia-yuzkiv <victoriayuzkiv@gmail.com>
2023-12-02 16:06:37 +0100

    Update tests

---
+++ test/test_feature_engineering.py
@@ -0,0 +1,132 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+# Import the Standardizer, Normalizer, Date, Encoding classes
+from fplibrary.feature_engineering import Standardizer, Normalizer, Date, Encoding
+
+
+class TestStandardizer(unittest.TestCase):
+
+    def setUp(self):
+        # Sample DataFrame for testing
+        data = {'Feature1': [1, 2, 3, 4, 5], 'Feature2': [10, 20, 30, 40, 50]}
+        self.df = pd.DataFrame(data)
+        self.standardizer_instance = Standardizer(self.df.copy())
+
+    def test_standardizer_transform(self):
+        # Test if the transform method runs without errors
+        cols_to_standardize = ['Feature1', 'Feature2']
+        self.standardizer_instance.transform(cols_to_standardize)
+
+        # Check if mean is approximately 0 and standard deviation is approximately 1 after standardization
+        for col in cols_to_standardize:
+            mean_after = np.mean(self.standardizer_instance.data[col])
+            std_after = np.std(self.standardizer_instance.data[col])
+            self.assertAlmostEqual(mean_after, 0, places=5)
+            self.assertAlmostEqual(std_after, 1, places=5)
+
+
+class TestNormalizer(unittest.TestCase):
+
+    def setUp(self):
+        # Create a sample DataFrame for testing
+        data = {'Feature1': [1, 2, 3, 4, 5], 'Feature2': [10, 20, 30, 40, 50]}
+        self.df = pd.DataFrame(data)
+        self.normalizer_instance = Normalizer(self.df)
+
+    def test_normalizer_transform(self):
+        # Specify the columns to normalize
+        cols_to_normalize = ['Feature1', 'Feature2']
+
+        # Perform normalization
+        self.normalizer_instance.transform(cols_to_normalize)
+
+        # Check if the normalization is correct
+        for col in cols_to_normalize:
+            min_value = min(self.df[col])
+            max_value = max(self.df[col])
+            normalized_values = [(value - min_value) / (max_value - min_value) for value in self.df[col]]
+
+            # Assert that the normalized values in the DataFrame match the expected values
+            self.assertTrue(np.allclose(self.df[col], normalized_values))
+
+
+class TestDate(unittest.TestCase):
+
+    def setUp(self):
+        # Create a sample DataFrame with a date column for testing
+        data = {'date_column': ['2023-01-01', '2023-02-15', '2023-03-30']}
+        self.df = pd.DataFrame(data)
+        self.date_instance = Date()
+
+    def test_split_date(self):
+        # Specify the column to split
+        column_to_split = 'date_column'
+
+        # Convert the 'date_column' to datetime format
+        self.df[column_to_split] = pd.to_datetime(self.df[column_to_split])
+
+        # Perform the split_date operation
+        self.date_instance.split_date(self.df, column_to_split)
+
+        # Check if new features 'day_of_week', 'month', and 'year' are created
+        self.assertTrue('day_of_week' in self.df.columns)
+        self.assertTrue('month' in self.df.columns)
+        self.assertTrue('year' in self.df.columns)
+
+        # Check if the values in the new features are as expected
+        expected_values = {'day_of_week': [6, 2, 3], 'month': [1, 2, 3], 'year': [2023, 2023, 2023]}
+        for feature, values in expected_values.items():
+            self.assertTrue(all(self.df[feature] == values))
+
+
+class TestEncoding(unittest.TestCase):
+
+    def setUp(self):
+        # Create a sample DataFrame for testing
+        data = {'Category': ['A', 'B', 'A', 'C', 'B', 'C'],
+                'Target': [1, 0, 1, 1, 0, 0]}
+        self.df = pd.DataFrame(data)
+        self.encoding_instance = Encoding(self.df.copy())
+
+    def test_mapping(self):
+        # Test the mapping method
+        mapping_dict = {'A': 1, 'B': 2, 'C': 3}
+        features = ['Category']
+        self.encoding_instance.mapping(features, mapping_dict)
+
+        # Check if values in the 'Category' column are mapped correctly
+        expected_values = [1, 2, 1, 3, 2, 3]
+        self.assertTrue(all(self.encoding_instance.data['Category'] == expected_values))
+
+    def test_one_hot_encoding(self):
+        # Test the one_hot_encoding method
+        feature = 'Category'
+        self.encoding_instance.one_hot_encoding(feature)
+
+        # Check if one-hot encoding is performed correctly
+        expected_columns = ['Target', 'Category_B', 'Category_C']
+        self.assertTrue(all(col in self.encoding_instance.data.columns for col in expected_columns))
+
+    def test_label_encoding(self):
+        # Test the label_encoding method
+        feature = 'Category'
+        self.encoding_instance.label_encoding(feature)
+
+        # Check if label encoding is performed correctly
+        expected_values = [0, 1, 0, 2, 1, 2]
+        self.assertTrue(all(self.encoding_instance.data['Category'] == expected_values))
+
+    def test_target_encoding(self):
+        # Test the target_encoding method
+        feature, target = 'Category', 'Target'
+        self.encoding_instance.target_encoding(feature, target)
+
+        # Check if target encoding is performed correctly
+        expected_values = [1.0, 0.0, 1.0, 0.5, 0.0, 0.5]
+        self.assertTrue(all(self.encoding_instance.data['Category'] == expected_values))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,6 +1,8 @@
 import unittest
 import pandas as pd
 import numpy as np
+from unittest.mock import patch
+from io import StringIO
 
 # Import the MissingValues class
 from fplibrary.preprocessing import MissingValues, Outliers
@@ -23,35 +25,115 @@ class TestMissingValues(unittest.TestCase):
         result_df = self.missing_values.remove_col(cols_to_remove)
         self.assertEqual(list(result_df.columns), ['Feature2', 'Feature3'])
 
-    def test_remove_nan(self):
+    def test_remove_rows_with_nan(self):
         # Test remove_nan method
         cols_with_nan = ['Feature1', 'Feature3']
-        result_df = self.missing_values.remove_nan(cols_with_nan)
+        result_df = self.missing_values.remove_rows_with_nan(cols_with_nan)
         self.assertEqual(result_df.shape, (3, 3))  # Assuming we have 5 rows initially
 
-    def test_fill_nan(self):
+    def test_remove_rows_with_nan_nonexistent(self):
+        # Test remove_nan method
+        cols_with_nan = ['Feature5']
+        with self.assertRaises(KeyError) as err:
+            self.missing_values.remove_rows_with_nan(cols_with_nan)
+
+            # Check the specific exception message or details if needed
+        expected_message = f"{cols_with_nan}"
+        self.assertEqual(str(err.exception), expected_message)
+
+    def test_fill_nan_with_mean(self):
         # Test fill_nan method
         cols_to_fill = ['Feature1', 'Feature3']
-        result_df = self.missing_values.fill_nan(cols_to_fill)
+        result_df = self.missing_values.fill_nan_with_mean(cols_to_fill)
         self.assertFalse(result_df[cols_to_fill].isnull().values.any())
 
+    def test_fill_nan_with_mean_values(self):
+        cols_to_fill = ['Feature1']
+
+        # Find indices of rows where any value is missing
+        missing_indices = self.df.index[self.df[cols_to_fill].isna().any(axis=1)]
+
+        # Perform the fill_nan_with_mean operation
+        self.missing_values.fill_nan_with_mean(cols_to_fill)
+
+        # Check if the values at missing indices are equal to the mean
+        for col in cols_to_fill:
+            mean_value = self.df[col].mean()
+            are_equal = np.all(self.missing_values.data.loc[missing_indices, col] == mean_value)
+            self.assertTrue(are_equal)
+
     def test_impute_missing_knn(self):
-        # Test impute_missing_knn method
         n_neighbors = 2
         result_df = self.missing_values.impute_missing_knn(n_neighbors)
         self.assertFalse(result_df.isnull().values.any())
 
     def test_missing_values_summary(self):
-        # Test for non-empty DataFrame
         summary = self.missing_values.missing_values_summary()
         self.assertIsInstance(summary, pd.DataFrame)
         self.assertEqual(summary.shape, (2, 2))
 
     def test_missing_values_summary_empty_df(self):
-        # Test for empty DataFrame
         empty_summary = self.empty_missing_values.missing_values_summary()
         self.assertIsInstance(empty_summary, pd.DataFrame)
         self.assertTrue(empty_summary.empty)
+
+
+class TestOutliers(unittest.TestCase):
+    def setUp(self):
+        # Create a sample DataFrame for testing
+        data = {'Feature1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                'Feature2': [15, 20, 25, 30, 35, 40, 45, 50, 55, 60],
+                'Feature3': [5, 5, 5, 5, 5, 5, 4, 7, 100, 123]}
+        self.df = pd.DataFrame(data)
+        self.outliers = Outliers(self.df.copy())
+
+    def test_detect_outliers_none(self):
+        # Create a StringIO object to capture printed output
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            # Run the detect_outliers method
+            self.outliers.detect_outliers(columns=['Feature1'])
+
+            # Get the printed output
+            printed_output = mock_stdout.getvalue().strip()
+
+        # Assert against the expected output
+        expected_output = "The number of outliers for Feature1 are 0."
+        self.assertIn(expected_output, printed_output)
+
+    def test_detect_outliers(self):
+        # Create a StringIO object to capture printed output
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            # Run the detect_outliers method
+            self.outliers.detect_outliers(columns=['Feature3'])
+
+            # Get the printed output
+            printed_output = mock_stdout.getvalue().strip()
+
+        # Assert against the expected output
+        expected_output = "The number of outliers for Feature3 are 2."
+        self.assertIn(expected_output, printed_output)
+
+    def test_winsorize(self):
+        # Check if the winsorize method runs without errors
+        self.outliers.winsorize(columns=['Feature1'])
+
+        # Check if the winsorized values are within expected limits
+        lower_limit = np.percentile(self.df['Feature1'], 5)
+        upper_limit = np.percentile(self.df['Feature1'], 95)
+
+        # Check if the winsorized values are within the expected limits
+        winsorized_values = self.outliers.data['Feature1'].values
+        self.assertTrue(all(winsorized_values >= lower_limit))
+        self.assertTrue(all(winsorized_values <= upper_limit))
+
+    def test_impute_with_null(self):
+        # Test if the impute_with_null method runs without errors
+        self.outliers.impute_with_null(columns=['Feature1'], below=3, above=8)
+        # Ensure that the imputation happened as expected
+        self.assertTrue(pd.isna(self.outliers.data['Feature1'][0]))
+        self.assertTrue(pd.isna(self.outliers.data['Feature1'][1]))
+        self.assertTrue(pd.isna(self.outliers.data['Feature1'][8]))
+        self.assertTrue(pd.isna(self.outliers.data['Feature1'][9]))
 
 
 if __name__ == '__main__':
