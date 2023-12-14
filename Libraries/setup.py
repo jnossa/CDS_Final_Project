dea8975cc16d4860dab3c26a34ceb3bd167f83a3
viktoriia-yuzkiv <victoriayuzkiv@gmail.com>
2023-12-14 16:42:10 +0100

    Update library imports and setup

--- Libraries/setup.py
+++ Libraries/setup.py
@@ -9,6 +9,11 @@ setup(
         'numpy>=1.20.3',
         'pandas>=1.2.4',
         'scipy>=1.7.3',
-        'matplotlib>=3.5.1'
+        'matplotlib>=3.5.1',
+        'seaborn',
+        'scikit-learn==1.3.1',
+        'setuptools==68.0.0',
+        'openmeteo-requests',
+        'requests-cache retry-requests'
     ],
 )
